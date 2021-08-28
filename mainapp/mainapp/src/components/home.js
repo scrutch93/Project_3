fa70e4d4e0f8fcd666da98aae38adcb38a59d90a
Rob Brunett <rb3@Robs-Air.attlocal.net>
2021-08-28 13:31:47 -0400

    branch update

---
+++ mainapp/mainapp/src/components/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './src/components/DelivRy_Logo';
+
+export default function homePage(){
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <button id="homeBtn">Login</button>
+        <br></br>
+        <button id="homeBtn">Create Account</button>
+      </header>   
+    </div>
+}
