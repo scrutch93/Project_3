42cb1abfa32b8b3b0916667a72e38c5f3e58e6f1
kferg357 <kennethferguson90@gmail.com>
2021-08-28 14:12:52 -0400

    added components and form folder with login page

---
+++ mainapp/mainapp/src/Components/Form/form.js
@@ -0,0 +1,44 @@
+import React from 'react'
+
+const Form = () => {
+    return (
+        <>
+            <div className="form-box">
+                <div class="button-box"></div>
+                <div id="btn"></div>
+                <button type="button" className="toggle-btn" onclick="login()">Log In</button>
+                <button type="button" className="toggle-btn" onclick="register()">Register</button>
+                <form id="login" className="input-group"></form>
+                <input type="text" className="input-field" placeholder="User name" required></input>
+                <input type="text" className="input-field" placeholder="Enter Password" required></input>
+                <input type="checkbox" className="check-box"><span>Remember Password</span></input>
+                <button type="submit" className="submit-btn">Log In</button>
+                <form id="register" className="input-group"></form>
+                <input type="text" className="input-field" placeholder="User name" required></input>
+                <input type="email" className="input-field" placeholder="Email Id" required></input>
+                {/* <!-- <input type="text" className="input-field" placeholder="Enter Passowrd" required> --> */}
+                <input type="checkbox" className="check-box"><span>I agree to the terms & conditions</span></input>
+                <button type="submit" className="submit-btn">Register</button>
+
+                <script>
+    var x = document.getElementById("login");
+    var y = document.getElementById("register");
+    var z = document.getElementById("btn");
+
+    function register(){
+      x.style.left = "-400px";
+      y.style.left = "50px";
+      z.style.left = "110px";
+    }
+    function login(){
+      x.style.left = "50px";
+      y.style.left = "450px";
+      z.style.left = "0";
+    }
+  </script>
+
+            </div>
+        </>
+        
+    )
+}
