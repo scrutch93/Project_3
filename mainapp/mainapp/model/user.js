935b4d66f9a29deec03619b692de7eaf259f891d
Steven Crutchfield <stevccru@icloud.com>
2021-08-31 19:13:40 -0400

    edited users.js

---
+++ mainapp/mainapp/model/user.js
@@ -0,0 +1,17 @@
+const mongoose = require ('mongoose')
+
+const UserSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true }
+    trackingNumber: { type: id, required: false}
+
+   
+  
+}
+{ collection: 'users' }
+)
+
+const model = mongoose.model('UserSchema', UserSchema)
+
+
+module.exports =model
