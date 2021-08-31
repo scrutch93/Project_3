eac9714b78af3214f09673afa4d67b084a3669f6
Rob Brunett <rb3@Robs-Air.lan>
2021-08-31 18:53:13 -0400

    user update

--- mainapp/mainapp/model/user.js
+++ mainapp/mainapp/model/user.js
@@ -2,9 +2,9 @@ const mongoose = require ('mongoose')
 
 const UserSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
-    password: { type: String, required: true }
+    password: { type: String, required: true },
     trackingNumber: { type: String, required: false}
-}
+},
 { collection: 'users' }
 )
 
