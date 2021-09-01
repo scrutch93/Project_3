399910c91cd401d98d2e180330f15c3a4c4954de
Rob Brunett <rb3@Robs-Air.lan>
2021-08-31 21:33:45 -0400

    list view update

--- mainapp/mainapp/src/App.js
+++ mainapp/mainapp/src/App.js
@@ -10,6 +10,19 @@ import './App.css';
 function App() {
 const [packageList, setTracking] = useState(data);
 
+const handleToggle = (id) => {
+  let mapped = packageList.map (tracking => {
+    return tracking.id == id ? {tracking, complete: !tracking.complete} 
+  })
+  setTracking(mapped);
+}
+
+const handleFilter = () => {
+  const filtered = packageList.filter(tracking => {
+    return !tracking.complete;
+  })
+  setTracking(filtered);
+}
   return (
     <div className="App">
       <Header />
