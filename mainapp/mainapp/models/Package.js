6f31357b5825ef1d0eb6a3876e757e9523ccf209
Steven Crutchfield <stevccru@icloud.com>
2021-09-04 20:53:07 -0400

    adeded information to package Schema

--- mainapp/mainapp/models/Package.js
+++ mainapp/mainapp/models/Package.js
@@ -1,10 +1,12 @@
 const { Schema, model } = require('mongoose');
 
 const PackageSchema = new Schema({
-    tracking_number: { type: Number},trim: true, required: "Please enter in a tracking number"
-    carrier: { type: String}, trim: true, 
+    tracking_number: { type: Number},trim: true, required: "Please enter in a tracking number",
+    carrier_code: { type: String}, trim: true, required: "Please select a carrier/carrier code",
     status_description: { type: String}, 
-    
+    ship_date: {type: String},
+    estimated_delivery_date: {type: String},
+    actual_delivery_date: {type: String}
 });
 
 
--- mainapp/mainapp/models/User.js
+++ mainapp/mainapp/models/User.js
@@ -6,7 +6,12 @@ const UserSchema = new Schema({
     username: { type: String, trim: true, required: "Please enter a username"},
     password: { type: String, trim: true, minlength: 6,required: "Password must contain at least 6 characters"},
     email: { type: String, unique: true, match: [/.+@.+\..+/, "Wuh-oh! That email address is not valid"]},
-    
+    packages: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Package'
+      }, 
+    ],   
 });
 
 UserSchema.pre('save', async function (next) {
