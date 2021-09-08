c7a254f704e33673695fe1e8ea2fa97975edd72c
kferg357 <kennethferguson90@gmail.com>
2021-09-08 12:36:13 -0400

    added user info

--- mainapp/mainapp/src/index.js
+++ mainapp/mainapp/src/index.js
@@ -11,6 +11,18 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+"carrier_id": "se-739693", STRING 
+"carrier_code": "stamps_com", STRING
+"account_number": "test_account_739693", STRING
+"requires_funded_amount": true, BOOLEAN
+"balance": 8888.9800, INTEGER
+"nickname": "ShipEngine Test Account - Stamps.com", STRING
+"friendly_name": "Stamps.com", STRING
+"primary": false, BOOLEAN
+"has_multi_package_supporting_services": false, BOOLEAN
+"supports_label_messages": true, BOOLEAn
+
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
