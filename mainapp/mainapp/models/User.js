474be41ff8eda64313bd32892ca6de50acc8f3bf
Rob Brunett <rb3@Robs-Air.lan>
2021-09-07 20:32:34 -0400

    update

--- mainapp/mainapp/models/User.js
+++ mainapp/mainapp/models/User.js
@@ -3,7 +3,7 @@ const { Schema, model } = require('mongoose');
 
 const UserSchema = new Schema({
     username: { type: String, trim: true, required: "Please enter a username"},
-    password: { type: String, trim: true, [(value) => {return value.length >=6}, "Password must contain at least 6 characters"},
+    password: { type: String, trim: true, [(value) => {return value.length >=6}]: "Password must contain at least 6 characters"},
     email: { type: String, unique: true, match: [/.+@.+\..+/, "Wuh-oh! That email address is not valid"]},
     
 });
